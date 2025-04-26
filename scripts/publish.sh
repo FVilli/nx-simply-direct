e675ae249e5b6731dc7227b73d8d19e696b0ad3d
Federico Villi <fvilli@eagleprojects.it>
2025-04-26 16:50:18 +0000

    [release]: v0.0.6

---
+++ scripts/publish.sh
@@ -0,0 +1,14 @@
+# Setup versione
+VERSION=$(npm version patch --no-git-tag-version)
+
+# Build
+npm run build
+
+# Commit e tag personalizzati
+git add .
+git commit -m "[release]: $VERSION"
+git tag "$VERSION"
+
+# Push
+git push
+git push --tags
