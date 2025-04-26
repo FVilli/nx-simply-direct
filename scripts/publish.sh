e5951633aae5e44d6a43f98f7f10055b2d8ffb9a
Federico Villi <fvilli@eagleprojects.it>
2025-04-26 17:09:53 +0000

    [release]: v0.0.8

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,12 @@
 # Setup versione
 VERSION=$(npm version patch --no-git-tag-version)
+VERSION_CLEAN=${VERSION#v}
 
 # Aggiorna tutti i package.json delle librerie
 for PACKAGE_JSON in libs/*/package.json
 do
-  echo "Aggiorno versione in $PACKAGE_JSON"
-  jq --arg v "$VERSION" '.version = $v' "$PACKAGE_JSON" > tmp.json && mv tmp.json "$PACKAGE_JSON"
+  echo "Version aligned for $PACKAGE_JSON"
+  jq --arg v "$VERSION_CLEAN" '.version = $v' "$PACKAGE_JSON" > tmp.json && mv tmp.json "$PACKAGE_JSON"
 done
 
 # Commit e tag personalizzati
