9f9da32f53126bd36db1bd9414e81c6f5114ef2a
Federico Villi <fvilli@eagleprojects.it>
2025-04-26 16:57:16 +0000

    [release]: v0.0.7

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,12 @@
 # Setup versione
 VERSION=$(npm version patch --no-git-tag-version)
 
-# Build
-npm run build
+# Aggiorna tutti i package.json delle librerie
+for PACKAGE_JSON in libs/*/package.json
+do
+  echo "Aggiorno versione in $PACKAGE_JSON"
+  jq --arg v "$VERSION" '.version = $v' "$PACKAGE_JSON" > tmp.json && mv tmp.json "$PACKAGE_JSON"
+done
 
 # Commit e tag personalizzati
 git add .
