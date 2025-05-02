70c82d496c133d5b5fb85b057901018287c973be
Federico Villi <fvilli@gmail.com>
2025-05-02 17:17:15 +0200

    [release] script publish with peerDeps

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Setup versione
 VERSION=$(npm version patch --no-git-tag-version)
 VERSION_CLEAN=${VERSION#v}
@@ -6,7 +8,25 @@ VERSION_CLEAN=${VERSION#v}
 for PACKAGE_JSON in libs/*/package.json
 do
   echo "Version aligned for $PACKAGE_JSON"
+  
+  # Aggiorna la versione principale del pacchetto
   jq --arg v "$VERSION_CLEAN" '.version = $v' "$PACKAGE_JSON" > tmp.json && mv tmp.json "$PACKAGE_JSON"
+  
+  # Aggiorna tutte le peerDependencies che iniziano con @simply-direct/
+  if jq -e 'has("peerDependencies")' "$PACKAGE_JSON" > /dev/null; then
+    echo "Updating peerDependencies for $PACKAGE_JSON"
+    
+    # Crea un file temporaneo con le peerDependencies aggiornate
+    jq --arg v "$VERSION_CLEAN" '
+      .peerDependencies |= with_entries(
+        if (.key | startswith("@simply-direct/")) then 
+          .value = "^" + $v
+        else 
+          .
+        end
+      )
+    ' "$PACKAGE_JSON" > tmp.json && mv tmp.json "$PACKAGE_JSON"
+  fi
 done
 
 # Commit e tag personalizzati
