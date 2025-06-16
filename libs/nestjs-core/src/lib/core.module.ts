c40df611e12a2e03cfbb9bf5b15a3c92f4eaaef7
Federico Villi <fvilli@eagleprojects.it>
2025-06-16 20:53:14 +0000

    [release]: v0.0.21

--- libs/nestjs-core/src/lib/core.module.ts
+++ libs/nestjs-core/src/lib/core.module.ts
@@ -3,22 +3,31 @@ import { CoreGateway, PrismaService } from './core.gateway';
 import { JwtService } from '@nestjs/jwt';
 
 export interface CoreModuleOptions {
-  port: number,
+  port?: number,
   databaseUrl: string,
-  JwtSecret: string,
-  JwtExpiresIn: string,
-  SkipAuth: boolean,
-  NotAllowedPrismaMethods: string[],
+  jwtSecret: string,
+  jwtExpiresIn?: string,
+  skipAuth?: boolean,
+  notAllowedPrismaMethods?: string[],
 }
+
+export const DEFAULT_OPTIONS: Partial<CoreModuleOptions> = {
+  port: 3000,
+  jwtExpiresIn: '7d',
+  skipAuth: false,
+  notAllowedPrismaMethods: ['deleteMany','updateMany']
+};
 
 @Module({})
 export class CoreModule {
   static forRoot(options: CoreModuleOptions): DynamicModule {
+    const useValue: CoreModuleOptions = { ...DEFAULT_OPTIONS, ...options };
+    console.log("CoreModule Options: ", useValue);
     return {
       module: CoreModule,
-      providers: [{ provide: 'CORE_MODULE_OPTIONS', useValue: options },CoreGateway,PrismaService,JwtService],
+      providers: [{ provide: 'CORE_MODULE_OPTIONS', useValue },CoreGateway,PrismaService,JwtService],
       exports: [CoreGateway],
-      global: true, // opzionale, rende il modulo globale
+      global: true,
     };
   }
 }
