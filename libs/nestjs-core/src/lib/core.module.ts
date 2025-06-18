0579c76afffa203fe5dd7f0db529633be68ec5f5
Federico Villi <fvilli@eagleprojects.it>
2025-06-18 20:17:19 +0000

    [release]: v0.0.24

--- libs/nestjs-core/src/lib/core.module.ts
+++ libs/nestjs-core/src/lib/core.module.ts
@@ -24,8 +24,7 @@ export const DEFAULT_OPTIONS:CoreModuleOptions = {
 export class CoreModule {
   static forRoot(options: Partial<CoreModuleOptions>): DynamicModule {
     const useValue: CoreModuleOptions = { ...DEFAULT_OPTIONS, ...options };
-    console.log("CoreModule Options:");
-    console.table(useValue);
+    console.log("CoreModule Options:",useValue);
     return {
       module: CoreModule,
       providers: [{ provide: 'CORE_MODULE_OPTIONS', useValue },CoreGateway,PrismaService,JwtService],
